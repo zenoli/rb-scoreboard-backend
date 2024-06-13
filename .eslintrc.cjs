35f297fdb8aff516b47bac8ea8c3003fd923ad81
Bitter Olivier <olivier.bitter@ti8m.ch>
2024-06-13 12:58:36 +0200

    Add eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:prettier/recommended', 'prettier', 'eslint:recommended'],
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  env: {
+    node: true,
+  },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+const app: Express = express()
+const port = process.env.PORT || 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("RB Scoreboard Backend");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('RB Scoreboard Backend')
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
