d727759932d7699800ea2c32e8247c0c1d612fa1
Bitter Olivier <olivier.bitter@ti8m.ch>
2024-06-14 20:02:17 +0200

    Add sportmonks types

---
+++ src/types/sportmonks.ts
@@ -0,0 +1,92 @@
+export interface Welcome {
+  data: Team[]
+  subscription: Subscription[]
+  rate_limit: RateLimit
+  timezone: string
+}
+
+export interface Team {
+  id: number
+  sport_id: number
+  country_id: number | null
+  venue_id: number | null
+  gender: Gender
+  name: string
+  short_code: null | string
+  image_path: string
+  founded: number | null
+  type: TeamType
+  placeholder: boolean
+  last_played_at: Date | null
+  players: Player[]
+}
+
+export enum Gender {
+  Male = "male",
+  Neutral = "neutral",
+}
+
+export interface Player {
+  id: number
+  transfer_id: null
+  player_id: number
+  team_id: number
+  position_id: number
+  detailed_position_id: number
+  start: null
+  end: Date | null
+  captain: boolean
+  jersey_number: number
+  player: PlayerInfo
+}
+
+export interface PlayerInfo {
+  id: number
+  sport_id: number
+  country_id: number
+  nationality_id: number
+  city_id: number | null
+  position_id: number
+  detailed_position_id: number
+  type_id: number | null
+  common_name: string
+  firstname: string
+  lastname: string
+  name: string
+  display_name: string
+  image_path: string
+  height: number
+  weight: number | null
+  date_of_birth: Date
+  gender: Gender
+}
+
+export enum TeamType {
+  Domestic = "domestic",
+  National = "national",
+}
+
+export interface RateLimit {
+  resets_in_seconds: number
+  remaining: number
+  requested_entity: string
+}
+
+export interface Subscription {
+  meta: Meta
+  plans: Plan[]
+  add_ons: any[]
+  widgets: any[]
+}
+
+export interface Meta {
+  trial_ends_at: null
+  ends_at: Date
+  current_timestamp: number
+}
+
+export interface Plan {
+  plan: string
+  sport: string
+  category: string
+}
