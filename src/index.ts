a37fdb1037711362d05d61dd13e6f946e87feeaf
olivier <olibitter@gmail.com>
2024-06-23 16:06:06 +0200

    Use internal mongodb url on railway.app

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,29 @@ import mongoose from "mongoose"
 import ImportsRouter from "./routes/imports"
 import ApiRouter from "./routes/api"
 
+function connectToMongoDB() {
+  const localDevelopment = process.env.STAGE === "local"
+  if (localDevelopment) {
+    console.log("Running locally. Using MONGO_URL")
+  } else {
+    console.log("Running on railway. Using MONGO_PRIVATE_URL")
+  }
+
+  // Use private network url when run on railway.app
+  const mongoUrl = localDevelopment
+    ? process.env.MONGO_URL
+    : process.env.MONGO_PRIVATE_URL
+  mongoose.connect(mongoUrl || "", {
+    dbName: "euro-2024",
+  })
+}
+
 dotenv.config()
+
+connectToMongoDB()
 
 const app: Express = express()
 const port = process.env.PORT || 3001
-
-const localDevelopment = process.env.STAGE === "local"
-if (localDevelopment) {
-  console.log("Running locally. Using MONGO_URL")
-} else {
-  console.log("Running on railway. Using MONGO_PRIVATE_URL")
-}
-
-// Use private network url when run on railway.app
-const mongoUrl = localDevelopment
-  ? process.env.MONGO_URL
-  : process.env.MONGO_PRIVATE_URL
-mongoose.connect(mongoUrl || "", {
-  dbName: "euro-2024",
-})
 
 app.get("/", (req: Request, res: Response) => {
   res.send("RB Scoreboard Backend")
