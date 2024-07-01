37f7ab630e534d6131f203b18541dbb44e79d539
olivier <olibitter@gmail.com>
2024-07-01 22:39:19 +0200

    Implement delete & insert for Types

--- src/imports/teams.ts
+++ src/imports/teams.ts
@@ -4,6 +4,7 @@ import { mapPlayer, mapTeam } from "../mappers"
 import { flatMap } from "lodash"
 import PlayerModel from "../models/player"
 import TeamModel from "../models/team"
+import { updateCollection } from "../utils/db"
 
 export async function importTeams() {
   const seasonId = process.env.SEASON_ID || "22842"
@@ -17,23 +18,7 @@ export async function importTeams() {
   const teamPlayers = flatMap(nationalTeams, (team) => team.players)
 
   await Promise.all([
-    TeamModel.bulkWrite(
-      nationalTeams.map((team) => ({
-        updateOne: {
-          filter: { _id: team.id },
-          update: mapTeam(team),
-          upsert: true,
-        },
-      }))
-    ),
-    PlayerModel.bulkWrite(
-      teamPlayers.map((teamPlayer) => ({
-        updateOne: {
-          filter: { _id: teamPlayer.player_id },
-          update: mapPlayer(teamPlayer),
-          upsert: true,
-        },
-      }))
-    ),
+    updateCollection(TeamModel, nationalTeams.map(mapTeam)),
+    updateCollection(PlayerModel, teamPlayers.map(mapPlayer)),
   ])
 }
