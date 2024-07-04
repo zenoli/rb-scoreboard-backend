376088aebf57d9449ae5fa08bdf02753fdc938b7
olivier <olibitter@gmail.com>
2024-07-04 23:06:56 +0200

    Fix initial import cron string

--- src/cronjobs/import-tasks.ts
+++ src/cronjobs/import-tasks.ts
@@ -10,7 +10,7 @@ import { format } from "date-fns"
 
 const EVERY_30_SECONDS = "*/30 * 17-23 * June,July *"
 const EVERY_30_SECONDS_SERVER = "*/30 * 15-21 * June,July *" // Account for different time offset on railway server
-const EVERY_DAY_SERVER = "* 4 * June,July *" // Account for different time offset on railway server
+const EVERY_DAY_SERVER = "0 4 * June,July *" // Account for different time offset on railway server
 const EVERY_10_SECONDS = "*/10 * * * * *"
 
 async function initialDataImportTask() {
